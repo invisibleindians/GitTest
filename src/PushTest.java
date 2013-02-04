dc6b58f0a235fe454aa5bc512c9b8966754fa1e6
invisibleindians <nancygabriel@gamil.com>
2013-02-04 11:35:04 -0600

    Update src/PushTest.java
    From Web

--- src/PushTest.java
+++ src/PushTest.java
@@ -1,6 +1,5 @@
-
 public class PushTest {
 	public static void main(String[] args) {
-	    System.out.println("Push Test Successful!!!!");
+	    System.out.println("Push Test Successful!!!! From Web");
 	  }
 }
