90258856c880da381331cee6930e02fa11188eae
invisibleindians <nancygabriel@gamil.com>
2013-02-04 12:43:49 -0600

    Update src/GitTest.java
    1111

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun.. now from local");
+	    System.out.println("Git is fun.. now from local 1111");
 	  }
 
 }
