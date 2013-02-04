6f000c2801a302ecfa483bab90ee1e817c1ea6d3
Invisible Indians <nancygabriel@gmail.com>
2013-02-04 12:52:17 -0600

    2222

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun.. now from local 1111");
+	    System.out.println("Git is fun.. now from local 1111222");
 	  }
 
 }
