6b13b77dd393fe29931a8ed06457765f8c44f0e6
Invisible Indians <nancygabriel@gmail.com>
2013-02-04 12:29:47 -0600

    From local now

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun.. From The web");
+	    System.out.println("Git is fun.. now from local");
 	  }
 
 }
